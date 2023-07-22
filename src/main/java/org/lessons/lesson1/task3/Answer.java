289d965d9d66f6d201dca7c6bc2699a1d72221eb
georgiy <georgii_sidenko@mail.ru>
2023-07-22 23:07:56 +0300

    task 3

---
+++ src/main/java/org/lessons/lesson1/task3/Answer.java
@@ -0,0 +1,14 @@
+package org.lessons.lesson1.task3;
+
+public class Answer {
+    public int[] divArrays(int[] a, int[] b){
+        if (a.length != b.length) {
+            return new int[]{0};
+        }
+        int[] c = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] / b[i];
+        }
+        return c;
+    }
+}
